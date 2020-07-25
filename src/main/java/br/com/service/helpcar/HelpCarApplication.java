b83bd9b02756166d3cc5bb4942072e8a18d161d7
JotaveOliveira <jv.oa202@gmail.com>
2020-07-25 19:31:19 -0300

    Subindo o projeto para o repositório git

---
+++ src/main/java/br/com/service/helpcar/HelpCarApplication.java
@@ -0,0 +1,13 @@
+package br.com.service.helpcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/service/helpcar/HelpCarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.service.helpcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
